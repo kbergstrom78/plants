5e48f5092067931f6015dc8997501620104e9c39
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-02-24 14:24:26 -0700

    Initial commit

---
+++ plants.rb
@@ -0,0 +1,13 @@
+#conditional statement about plants
+#define variable
+# if statement - sunshine = true, oz of water = 10 print "plant will thrive"
+# any other answer and "plant will die" 
+
+gets_sunshine = false
+ounces_of_water = 1
+
+if gets_sunshine && ounces_of_water == 10
+    puts "Plant will thrive"
+else 
+    puts "Plant will die"
+end   
